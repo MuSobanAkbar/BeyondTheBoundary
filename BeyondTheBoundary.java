0a6bccdf61532f27975994b49e38f1b894f1342e
MuSobanAkbar <mu.sobanakbar@gmail.com>
2025-04-02 14:10:29 -0300

    Add files via upload
    The game now works, and is only working with bias possibilities. It also lacks the feature of chasing the runs down or the second over, only one over is being played.

--- BeyondTheBoundary.java
+++ BeyondTheBoundary.java
@@ -1,5 +1,6 @@
 import java.util.Random;
 import java.util.Scanner;
+
 public class BeyondTheBoundary
 {
 	public static void main(String [] args)
@@ -8,25 +9,36 @@ public class BeyondTheBoundary
 		int userRuns = 0;
 		int cpuRuns = 0;
 		int wicketsUser = 0;
+		boolean userStarting = false;
+		String f ="";
+		String userChoice ="";
 		System.out.println("Heads or Tails? (H/T): ");
 		int choice = 0;
 		String tossC = scanner.nextLine().toUpperCase();
 		if(tossC.equals("H"))
 		{
 			choice=0;
+			f = "Heads";
 		}
 		else if(tossC.equals("T"))
 		{
 			choice=1;
+			f="Tails";
 		}
 		Random r = new Random();
 		int possibility = r.nextInt(2);
+
 		if(possibility == choice)
 		{
+
+			userStarting=true;
 			System.out.println("You have won the toss!");
+			System.out.println("What would you like to pick (BAT/BOWL)?: ");
+			userChoice = scanner.nextLine().toUpperCase();
 		}
 		else
 		{
+			userStarting=false;
 			if(possibility==1)
 			{
 				tossC = "bat";
@@ -39,12 +51,12 @@ public class BeyondTheBoundary
 		}
 		boolean out=false;
 
-		if(tossC.equals("bowl"))
+		if(tossC.equals("bat")||userChoice.equals("BOWL"))
 		{
-			for(int i=0;i<7&&!out;i++)
+			for(int i=0;i<6&&!out;i++)
 			{
 				System.out.println("Total Runs by CPU:" + cpuRuns +"\nBalls Left: " + (6-i)+"\nTotal Wickets: ");
-				System.out.println("Please throw your ball "+ (i+1) + " (type 'throw')");
+				System.out.println("Please throw your ball "+ (i+1) + " (type 'throw'): ");
 				String throwB = scanner.nextLine();
 				if(throwB.equals("throw"))
 				{
@@ -69,6 +81,31 @@ public class BeyondTheBoundary
 			}
 			System.out.println("Over finished.");
 		}
+		else if(tossC.equals("bowl")||userChoice.equals("BAT"))
+		{
+			for(int i =0;i<6&&!out;i++)
+			{
+				System.out.println("Total Runs by you:" + userRuns +"\nBalls Left: " + (6-i)+"\nTotal Wickets: ");
+				System.out.println("Please pick a batting style for ball "+ (i+1) + " (type 'strong'/'defense'): ");
+				String run = scanner.nextLine().toLowerCase();
+				if(run.equals("strong"))
+				{
+					int runsU = r.nextInt(4 - 7)+4;
+					userRuns+=runsU;
+				}
+				else if(run.equals("defense"))
+				{
+					int runsU = r.nextInt(0-5)+5;
+					userRuns+=runsU;
+				}
+				else
+				{
+					System.out.println("Oops, you've been bowled.");
+					out=true;
+				}
+			}
+		}
+		System.out.println("Over finished.");
 
 	}
 }
