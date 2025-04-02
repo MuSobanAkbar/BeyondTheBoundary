4b69495a3480825f331c0c0d721277d94fe65da5
MuSobanAkbar <mu.sobanakbar@gmail.com>
2025-04-02 10:59:14 -0300

    Add files via upload
    Added functionality to the toss feature, now an initialisation included.

--- BeyondTheBoundary.java
+++ BeyondTheBoundary.java
@@ -4,16 +4,35 @@ public class BeyondTheBoundary
 {
 	public static void main(String [] args)
 	{
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Heads or Tails? (H/T): ");
+		int choice = 0;
+		String tossC = scanner.nextLine().toUpperCase();
+		if(tossC.equals("H"))
+		{
+			choice=0;
+		}
+		else if(tossC.equals("T"))
+		{
+			choice=1;
+		}
 		Random r = new Random();
 		int possibility = r.nextInt(2);
-
-		if(possibility == 1)
+		if(possibility == choice)
 		{
-			System.out.println("You are batting");
+			System.out.println("You have won the toss!");
 		}
 		else
 		{
-			System.out.println("You are bowling");
+			if(possibility==1)
+			{
+				tossC = "bat";
+			}
+			else
+			{
+				tossC = "bowl";
+			}
+			System.out.println("You have lost the toss, the opposing team has decided to "+ tossC);
 		}
 	}
 }
